fc4b257733c633aa5df48e98da7726d76dc3ebdd
Bob Wombat Hogg <wombat@rwhogg.site>
2019-06-08 11:37:45 -0700

    Update packages to support Node >= 10
    Signed-off-by: Bob Wombat Hogg <wombat@rwhogg.site>

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
-const gulp = require("gulp");
-const jsValidate = require(".");
+const gulp = require("gulp")
+const jsValidate = require(".")
 
-gulp.task("default", () =>
-    ["valid-es6.js", "invalid-es6.js", "valid-es5.js", "invalid-es5.js"].forEach(file => {
-        gulp.src(file)
+gulp.task("default", () => {
+    const files = ["valid-es6.js", "invalid-es6.js", "valid-es5.js", "invalid-es5.js"]
+    return Promise.all(
+        files.map(file => {
+        return gulp.src(file)
             .pipe(jsValidate())
             .on("error", error => {
-                console.error("Message: " + error.message);
-                console.error("Location: " + error.fileName);
-            });
-    })
-);
+                console.error("Message: " + error.message)
+                console.error("Location: " + error.fileName)
+            })
+        })
+    )
+})
--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-import test from 'ava';
-import Vinyl from 'vinyl';
-import m from '.';
+import test from "ava"
+import Vinyl from "vinyl"
+import m from "."
 
 test.cb("Basic test", t => {
 	t.plan(1);
 
 	const stream = m();
 
-	stream.on('error', () => {
-		t.pass();
-		t.end();
+	stream.on("error", () => {
+		t.pass()
+		t.end()
 	});
 
 	stream.end(new Vinyl({
-		contents: Buffer.from('const foo = \'bar;')
+		contents: Buffer.from("const foo = 'bar;")
 	}));
 });
